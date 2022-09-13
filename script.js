19811deccb56779972d12f2a64328f11bc08921a
Kipras Čižauskas <kipraschi@gmail.com>
2022-09-13 23:57:52 +0300

    feat: add render method
    _render method in the displayController module displays the gameboard array in the DOM

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-// const game = (function () {
-
+// const Game = (function () {
 const NewPlayer = function (myName, myMark) {
 	const name = () => myName;
 	const mark = () => myMark;
-	return {name, mark};
+	const makeMove = () => {}; //spits out an index where their mark should go?
+	return {name, mark, makeMove};
 };
 
 const p1 = NewPlayer("Tim", "X");
 const p2 = NewPlayer("Bob", "0");
 
 const gameBoard = (function () {
-    var gameboard = [];
-    return {gameboard}
+	var gameboard = ["X", "0", "X", "X", "X", "0", "0", "X", "0"];
+	return {gameboard};
 })();
 
-const displayController = (function () {
-	const _render = () => {};
-    const _bindEvents = () => {};
-})();
+const displayController = (function (board) {
+	const _render = () => {
+        document.querySelector("ul").remove();
+		const grid = document.createElement("ul");
+		board.forEach((mark) => {
+            let box = document.createElement("li");
+            box.textContent = `${mark}`;
+			grid.appendChild(box);
+		});
+        document.querySelector(".grid-container").appendChild(grid);
+	};
+	const _bindEvents = () => {};
+
+    return {_render};
+})(gameBoard.gameboard);
 
 // })();
