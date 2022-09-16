2801e4054fd1e92fba8566451ea8fd93c2a80f0e
Kipras Čižauskas <kipraschi@gmail.com>
2022-09-16 23:15:42 +0300

    feat: add game over states
    IsWon() and isDraw() methods check whether the game is over

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 const Game = (function () {
-
 	const NewPlayer = function (myName, myMark) {
 		const name = () => myName;
 		const mark = () => myMark;
 		const makeMove = (index) => {
-			if (!Gameboard.gameboard[index]) Gameboard.gameboard[index] = mark();
+			if (!Gameboard.gameboard[index] && !isWon() && !isDraw())
+				Gameboard.gameboard[index] = mark();
+			else isDraw();
 		};
 		return {name, mark, makeMove};
 	};
 
-	const p1 = NewPlayer("Tim", "X");
-	const p2 = NewPlayer("Bob", "0");
-	let turn = 1;
-
-	const switchPlayer = function () {
-		if (turn % 2 == 0) currentPlayer = p1;
-		else currentPlayer = p2;
-		turn++;
-		return currentPlayer;
-	};
-
 	const Gameboard = (function () {
 		var gameboard = ["", "", "", "", "", "", "", "", ""];
+
 		return {gameboard};
 	})();
 
-	const DisplayController = (function (board) {
+	const DisplayController = (function () {
 		const _bindEvents = (tile, index) => {
 			tile.addEventListener("click", () => {
 				switchPlayer().makeMove(index);
@@ -35,10 +26,9 @@ const Game = (function () {
 
 		const _render = function () {
 			document.querySelector("ul").remove();
-
 			const grid = document.createElement("ul");
 
-			board.forEach((mark, index) => {
+			Gameboard.gameboard.forEach((mark, index) => {
 				let tile = document.createElement("li");
 				tile.textContent = `${mark}`;
 				_bindEvents(tile, index);
@@ -48,5 +38,34 @@ const Game = (function () {
 		};
 
 		_render();
-	})(Gameboard.gameboard);
+	})();
+
+	const p1 = NewPlayer("Tim", "X");
+	const p2 = NewPlayer("Bob", "0");
+	let turn = 1;
+
+	function switchPlayer() {
+		if (turn % 2) currentPlayer = p1;
+		else currentPlayer = p2;
+		turn++;
+
+		return currentPlayer;
+	}
+
+	function isWon() {
+		let board = Gameboard.gameboard;
+		return (
+			(board[0] === board[1] && board[1] === board[2] && board[2] !== "") ||
+			(board[3] === board[4] && board[4] === board[5] && board[5] !== "") ||
+			(board[6] === board[7] && board[7] === board[8] && board[8] !== "") ||
+			(board[0] === board[3] && board[3] === board[6] && board[6] !== "") ||
+			(board[1] === board[4] && board[4] === board[7] && board[7] !== "") ||
+			(board[2] === board[5] && board[5] === board[8] && board[8] !== "") ||
+			(board[0] === board[4] && board[4] === board[8] && board[8] !== "") ||
+			(board[2] === board[4] && board[4] === board[6] && board[6] !== "")
+		);
+	}
+	function isDraw() {
+		return !isWon() && Gameboard.gameboard.every((element) => element !== "");
+	}
 })();
