0a4482fcda535f07e1b10cb6ede72fa199ea6ae5
Kipras Chi <kipraschi@gmail.com>
2026-02-28 21:22:11 +0200

    feat: define the player class with a cell selection method

---
+++ lib/player.rb
@@ -0,0 +1,23 @@
+class Player
+
+  attr_reader :mark
+
+  def initialize(mark)
+    @mark = mark
+  end
+
+  def select_cell
+    cell_selected = nil
+    until (1..9).include?(cell_selected)
+      print "Enter a number 1 to 9 to place your mark: "
+      begin
+        cell_selected = Integer(gets)
+        raise unless (1..9).include?(cell_selected)
+      rescue
+        puts "That's not a number 1 to 9!"
+      end
+    end
+    cell_selected - 1
+  end
+  
+end
