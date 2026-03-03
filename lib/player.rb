d94c3bd1e99973aef799699beeb53edfa4d7a1c4
Kipras Chi <kipraschi@gmail.com>
2026-03-03 21:50:32 +0200

    refactor: separate input validation in player selection, rename local variable

--- lib/player.rb
+++ lib/player.rb
@@ -7,17 +7,21 @@ class Player
   end
 
   def select_cell
-    cell_selected = nil
-    until (1..9).include?(cell_selected)
-      begin
-        cell_selected = Integer(gets)
-        raise unless (1..9).include?(cell_selected)
-      rescue
-        puts "That's not a number 1 to 9!"
-        print "Choose another number from 1 to 9 to make your move: "
-      end
+    begin
+      selection = Integer(gets)
+      raise unless valid?(selection)
+    rescue
+      puts "That's not a number 1 to 9!"
+      print "Choose another number from 1 to 9 to make your move: "
+      retry
     end
-    cell_selected - 1
+    selection - 1
+  end
+
+  private 
+
+  def valid?(input)
+    (1..9).include?(input)
   end
 
 end
