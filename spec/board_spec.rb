bb5bf56b376492e0746b63332bbb2960565c1b07
Kipras Chi <kipraschi@gmail.com>
2026-05-24 09:19:43 +0300

    test: add unit tests for Board#cell_empty?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Board do
       expect(board.board[5]).to eq('X')
     end
   end
+  describe '#cell_empty?' do
+    context 'when the cell is empty' do
+      it 'returns true' do
+        expect(board.cell_empty?(7)).to be true
+      end
+    end
+    context 'when the cell is full' do
+      it 'returns false' do
+        board.update(1, 'O')
+        expect(board.cell_empty?(1)).to be false
+      end
+    end
+  end
 end
